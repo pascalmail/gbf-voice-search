928ba73b7960980cc6cd80b7592da8cd113b1902
Ismail Hassan <ismail.bukittinggi@gmail.com>
2016-09-11 14:37:21 +0700

    Fix naming and others.

--- check_voice.sh
+++ check_voice.sh
@@ -56,38 +56,38 @@ echo "</tr>"
 
 # {ID}_v_(\d\d\d)a{0,1}\.mp3
 # {ID}_v_(\d\d\d)_(a|b)\.mp3
-# for i in $(seq -f "%03g" 1 599); do
-#     URLP2="_v_$i"
-#     URL1=$URLP1$CHARA_ID$URLP2$EXT
-#     URL2=$URLP1$CHARA_ID$URLP2"a"$EXT
+for i in $(seq -f "%03g" 1 599); do
+    URLP2="_v_$i"
+    URL1=$URLP1$CHARA_ID$URLP2$EXT
+    URL2=$URLP1$CHARA_ID$URLP2"a"$EXT
 
-#     URL3=$URLP1$CHARA_ID$URLP2"_a"$EXT
-#     URL4=$URLP1$CHARA_ID$URLP2"_b"$EXT
-#     FOUND="NO"
-#     if isValidURL $URL1; then
-#         echoerr "FOUND $URL1"
-#         writeList $URL1 $URLP2
-#         FOUND="YES"
-#     fi
-#     if isValidURL $URL2; then
-#         echoerr "FOUND $URL2"
-#         writeList $URL2 $URLP2"a"
-#         FOUND="YES"
-#     fi
-#     if isValidURL $URL3; then
-#         echoerr "FOUND $URL3"
-#         writeList $URL3 $URLP2"_a"
-#         FOUND="YES"
-#     fi
-#     if isValidURL $URL4; then
-#         echoerr "FOUND $URL4"
-#         writeList $URL4 $URLP2"_b" 
-#         FOUND="YES"
-#     fi 
-#     if [ $FOUND="NO" ]; then
-#         echoerr "NONE $URL1"
-#     fi
-# done
+    URL3=$URLP1$CHARA_ID$URLP2"_a"$EXT
+    URL4=$URLP1$CHARA_ID$URLP2"_b"$EXT
+    FOUND="NO"
+    if isValidURL $URL1; then
+        echoerr "FOUND $URL1"
+        writeList $URL1 $URLP2
+        FOUND="YES"
+    fi
+    if isValidURL $URL2; then
+        echoerr "FOUND $URL2"
+        writeList $URL2 $URLP2"a"
+        FOUND="YES"
+    fi
+    if isValidURL $URL3; then
+        echoerr "FOUND $URL3"
+        writeList $URL3 $URLP2"_a"
+        FOUND="YES"
+    fi
+    if isValidURL $URL4; then
+        echoerr "FOUND $URL4"
+        writeList $URL4 $URLP2"_b" 
+        FOUND="YES"
+    fi 
+    if [ $FOUND="NO" ]; then
+        echoerr "NONE $URL1"
+    fi
+done
 
 
 # {ID}_mypage(\d)a{0,1}\.mp3
